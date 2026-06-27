5c0101b929f93ae64dc49ad417cb2b0b11aa46eb
jaypanchal-sde <jaypanchal2175@gmail.com>
2026-06-28 00:30:55 +0530

    fix upload patch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export {ConfigManager} from "./config/ConfigManager";
-export {UploadMiddleware} from "./middleware/UploadMiddleware";
 export type {UploadConfig} from "./@types/config.types";
 export { DecryptionEngine } from './crypto/DecryptionEngine';
 export { DownloadController } from './middleware/DownloadController';
